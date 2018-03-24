5c09e3cd81b604a6b4d121dbf7007fbc788dee2a
Leandro Tk <leandrotk100@gmail.com>
2018-03-24 17:11:38 -0300

    List comprehension with else

--- tricks/list_comprehension.py
+++ tricks/list_comprehension.py
@@ -7,10 +7,33 @@ for x in range(10):
 for val in vals:
     print(val)
 
-print()
+print('--------------------')
 
 # list comprehension
 vals = [x * x for x in range(10) if x % 2 != 0]
+
+for val in vals:
+    print(val)
+
+print('--------------------')
+
+# list comprehension with else
+
+objs = ['hi', 'hello', 'mom', None, 'dad', 'sad']
+vals = []
+
+for obj in objs:
+    if obj is not None:
+        vals.append(obj)
+    else:
+        vals.append('')
+
+for val in vals:
+    print(val)
+
+print('--------------------')
+
+vals = [obj if obj is not None else '' for obj in objs]
 
 for val in vals:
     print(val)
